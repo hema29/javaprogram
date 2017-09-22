0dae729ae79ca376872f6f024e8f7943898d9272
hema29 <31930095+hema29@users.noreply.github.com>
2017-09-22 15:45:57 +0530

    Add files via upload

---
+++ Hello World.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class JavaProgram
+{
+    public static void main(String args[])
+    {
+		for (int i=0; i<5; i++)
+	
+       System.out.println("Hello World!");
+	   
+    }
+}
